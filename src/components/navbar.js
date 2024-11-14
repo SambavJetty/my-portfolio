c656a48b5baef254301e6ccb2125afacff6293f7
SambavJetty <140359924+SambavJetty@users.noreply.github.com>
2024-11-14 10:35:56 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,7 +11,7 @@ function navbar() {
             <Link to="/experience">Experience</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/certifications">Certifications</Link>
-            <a href='https://drive.google.com/file/d/13SNGNMOjkKrWQdSmv_7m4aHdtZg1KGvs/view?usp=drive_link' style={{textDecoration:'none'}}>Resume</a>
+            <a href='https://drive.google.com/file/d/1Ok9RbBRr3mBrLwLSxiu7w6FzcMxTqqF6/view?usp=sharing' style={{textDecoration:'none'}}>Resume</a>
 
         </div>
       
