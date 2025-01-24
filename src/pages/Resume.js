63176dc74f1e680bb7ca140e9e0d1356eab62153
SambavJetty <140359924+SambavJetty@users.noreply.github.com>
2025-01-24 09:27:40 +0530

    Update Resume.js

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Resume = () => {
   return (
     <div>
-      <img src=''></img>
+      <img src='https://drive.google.com/file/d/1lGTXf9VpHY5TfzXZPVteOfAX4fkYw6jf/view?usp=sharing'></img>
     </div>
   )
 }
